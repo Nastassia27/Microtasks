b7586ca0598695e2c199642922753d42b7be552f
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-06 20:13:36 +0300

    useState microtask

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Header} from "./site/Header";
@@ -9,31 +9,23 @@ import {MouseEvent} from 'react';
 import {Button} from "./components/Button";
 
 function App() {
-    /*const myFirstSubscriber = (event: number) => {
-        console.log('Hello im Vasya')
-    }
-    const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-        console.log('Hello im Ivan')
-    }*/
+   // let a = 1
+    let [a, setA] = useState(1);
+    const onClickHandler = () => {
+        setA(++a);
 
-const Button1Foo = (subscriber: string, age:number)=>{
-    console.log(subscriber, age)
-}
-    const Button2Foo = (subscriber: string, age:number)=>{
-        console.log(subscriber, age)
+        console.log(a)
     }
-    const Button3Foo = ()=>{
-        console.log("im stupid button")
+
+    const deleteOnCLick = ()=>{
+        setA(0);
     }
+
     return (
         <div className='App'>
-
-            <Button name={'YouTubeChannel-1'} callBack={()=> Button1Foo('im vasya',21)}/>
-            <Button name={'YouTubeChannel-2'} callBack={()=>Button2Foo('im ivan',22)}/>
-            <Button name={'im stupid button'} callBack={Button3Foo}/>
-
-
-
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={deleteOnCLick}>0</button>
         </div>
     );
 }
