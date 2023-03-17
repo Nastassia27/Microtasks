6ba5ca09290785fde955a00af72e84b453a43b89
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-17 23:36:34 +0300

    input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import React, {ChangeEvent, useState} from "react";
+
+
+type InputType= {
+    title: string,
+        setTitle: (title: string)=> void
+}
+export const Input = (props: InputType)=>{
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        props.setTitle(event.currentTarget.value);
+
+
+    }
+
+    return(
+        <input value={props.title} onChange={onChangeInputHandler}/>
+    )
+}
