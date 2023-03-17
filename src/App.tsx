fb4efd2da8b12ab33df61842c5675433e81c5dbc
Nastassia27 <anastasia.derugo@gmail.com>
2023-03-17 23:37:45 +0300

    input microtask

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,48 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 import './App.css';
-/*import {Header} from "./site/Header";
-import logo from './logo.svg';
-import {Body} from "./site/Body";
-import {Footer} from "./site/Footer";
-import {NewComponent} from "./Map/NewComponent";
-import {MouseEvent} from 'react';
-import {Button} from "./components/Button";*/
-import {FinalMicrotask, MoneyObjTypes} from "./FinalMicrotask";
-
-export type FilterType = 'All' | 'Dollars' | 'RUBLS';
-function App() {
+import {FullInput} from "./components/FullInput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 
+    function App() {
+        const [message, setMessage] = useState([
+                {message: 'message1'},
+                {message: 'message2'},
+                {message: 'message3'},
 
-    const [money, setMoney] = useState<Array<MoneyObjTypes>>([
-        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-    ])
-
-    const [filter, setFilter] = useState<FilterType>('All')
-
-    let currentMoney = money
-    if (filter === 'RUBLS') {
-        (
-            currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
+            ]
         )
-    }
-    if (filter === 'Dollars') {
-        (
-            currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
-        )
-    }
 
-    const onClickFilterHandler = (value: FilterType) => {
-        setFilter(value);
+        let [title, setTitle] = useState('')
 
-    }
+        const addMessage = (title: string)=>{
+            let newMessage={message: title}
+            setMessage([newMessage,...message])
 
+        }
 
-    return (
-        <FinalMicrotask money={currentMoney} onClickFilterHandler={onClickFilterHandler}/>
-        /*<div>
-            <ul>
-                {currentMoney.map((objFromMoneyArr, index) => {
+        const callBackButtonHandler = ()=>{
+            addMessage(title)
+            setTitle('')
+        }
+        return (
+            <div className="App">
+                <Input setTitle={setTitle} title={title}/>
+               <Button name={'Butt'} callBack={()=>{callBackButtonHandler()}} />
+                {/*<div>
+                    <input />
+                    <button>+</button>
+                </div>*/}
+                {message.map((el, index) => {
                     return (
-                        <li key={index}>
-                            <span>{objFromMoneyArr.banknots}</span>
-                            <span>{objFromMoneyArr.value}</span>
-                            <span>{objFromMoneyArr.number}</span>
-                        </li>
+                        <div key={index}>{el.message}</div>
                     )
                 })}
-            </ul>
-            <div style={{marginLeft: '35px'}}>
-                <button onClick={() => onClickFilterHandler('All')}>All</button>
-                <button onClick={() => onClickFilterHandler('RUBLS')}>Ruble</button>
-                <button onClick={() => onClickFilterHandler('Dollars')}>Dollar</button>
             </div>
-        </div>*/
-    );
+        );
+
 }
 
 export default App;
